dd2ed473f7f311c93b5c775683d3b999f2c9d2d8
Paulina G <paulinag@Paulinas-MacBook-Pro.local>
2024-07-29 14:26:17 +0100

    recipie update

--- recipes.js
+++ recipes.js
@@ -1,3 +1,4 @@
+import { error } from "node:console";
 import * as fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
 
@@ -5,54 +6,77 @@ const fileName = "recipes.json";
 
 // GET ALL RECIPES
 export async function getRecipes() {
-	try {
-		let db = await fs.readFile(fileName, "utf-8");
-		let parsedDb = JSON.parse(db);
-		return parsedDb;
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    let db = await fs.readFile(fileName, "utf-8");
+    let parsedDb = JSON.parse(db);
+    return parsedDb;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // GET A RECIPE BY ID
 export async function getRecipeByID(id) {
-	try {
-		let db = await fs.readFile(fileName, "utf-8");
-		let parsedDb = JSON.parse(db);
+  try {
+    let db = await fs.readFile(fileName, "utf-8");
+    let parsedDb = JSON.parse(db);
 
-		return parsedDb.find((el) => {
-			return el.id === id;
-		})
-
-	} catch (error) {
-		console.error(error);
-	}
+    return parsedDb.find((el) => {
+      return el.id === id;
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // CREATE A RECIPE
 export async function createRecipe(newRecipe) {
-	try {
-		let db = await fs.readFile(fileName, "utf-8")
-		let parsedDb = JSON.parse(db);
-		if (newRecipe.title && newRecipe.ingredients && newRecipe.instructions && newRecipe.image
-		) {
-			parsedDb.push({
-				...newRecipe,
-				id: uuidv4()
-			})
-		} else {
-			return null
-		}
+  try {
+    let db = await fs.readFile(fileName, "utf-8");
+    let parsedDb = JSON.parse(db);
+    if (
+      newRecipe.title &&
+      newRecipe.ingredients &&
+      newRecipe.instructions &&
+      newRecipe.image
+    ) {
+      parsedDb.push({
+        ...newRecipe,
+        id: uuidv4(),
+      });
+    } else {
+      return null;
+    }
 
-		const returnedDb = JSON.stringify(parsedDb)
-		return fs.writeFile(fileName, returnedDb, "utf-8")
-	} catch (error) {
-		console.log(error.message)
-	}
+    const returnedDb = JSON.stringify(parsedDb);
+    return fs.writeFile(fileName, returnedDb, "utf-8");
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 // UPDATE A RECIPE BY ID
-export async function updateRecipeByID(id, updatedRecipe) { }
+export async function updateRecipeByID(id, updatedRecipe) {
+  try {
+    let db = await fs.readFile(fileName, "utf-8");
+    let parsedDb = JSON.parse(db);
+    let index = recipes.findIndex((recipe) => recipe.id === id);
+    if (index === -1) {
+      throw new Error("Recipe not found");
+    }
+    parsedDb[index] = {
+      id: id,
+      title: updatedRecipe.title,
+      ingredients: updatedRecipe.ingredients,
+      instructions: updatedRecipe.ingredients,
+      image: updatedRecipe.image,
+    };
+    await fs.writeFile(fileName, JSON.stringify(parsedDb), "utf-8");
+    return parsedDb[index];
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // DELETE A RECIPE BY ID
-export async function deleteRecipeByID(id) { }
+export async function deleteRecipeByID(id) {}
