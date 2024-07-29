2369b0852e1fa4ef03ea82c09774bc9030d17c77
kieranp-p6 <kiip96@icloud.com>
2024-07-29 11:12:36 +0100

    jnkbksbb

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from 'cors';
+
+
 
 import {
   getRecipes,
@@ -9,14 +12,16 @@ import {
 } from "./recipes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
+app.use(cors());
 
 app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/api/recipes", async (req, res) => {
   let allRecipes = await getRecipes();
-	res.status(200).json({
+  res.status(200).json({
     success: true,
     payload: allRecipes
   });
